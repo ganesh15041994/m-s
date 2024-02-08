46b95582117e9780a3b1105b50adeb8d7253ade4
ganesh <ganeshrat1994@gmail.com>
2024-02-08 16:49:22 +0530

    commeting

---
+++ src/main/java/com/appsdeveloperblog/photoapp/api/ApiSpringGatwayAppApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringGatwayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringGatwayAppApplication.class, args);
+	}
+
+}
